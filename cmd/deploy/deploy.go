a0c48160f45fbf9790ad6ced41770d2593a4e69e
Stefan Kürzeder <stefan.kuerzeder@gmail.com>
2020-04-30 12:51:03 +0200

    Cleanup

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -28,14 +28,7 @@ import (
 
 // deployCmd represents the deploy command
 var Cmd = &cobra.Command{
-	Use:   "deploy",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use: "deploy",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		flags := cmd.Flags()
 		sp, err := azure.GetServicePrincipal(flags)
--- cmd/root.go
+++ cmd/root.go
@@ -29,17 +29,7 @@ var v string
 
 // rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "azure-arm-action",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Use: "azure-arm-action",
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		if err := setUpLogs(os.Stdout, v); err != nil {
 			return err
